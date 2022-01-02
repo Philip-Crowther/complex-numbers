263eb13ed9a80ebf6638c7531461f8743a946a21
Philip Crowther <crowtherpb@gmail.com>
2022-01-02 15:36:02 -0500

    build skeleton

--- complex_number.py
+++ complex_number.py
@@ -2,17 +2,40 @@
 
 class ComplexNumber:
     """representation of a complex number"""
-    def __init__(self, real_part, imaginary_part):
+    def __init__(self, real_part=0, imaginary_part=0):
         self.real = real_part
         self.imaginary = imaginary_part
+
+    @property
+    def angle(self):
+        pass
+
+    @property
+    def magnitude(self):
+        pass
 
     def __repr__(self):
         return f"ComplexNumber(real='{self.real}', imaginary={self.imaginary})"
 
     def __str__(self) -> str:
         return f"{self.real} + {self.imaginary}i"
-    
 
+    def __add__(self, addend):
+        pass
+
+    def __sub__(self, subtrahend):
+        pass
+
+    def __mul__(self, factor):
+        pass
+
+    def __truediv__(self, divisor):
+        pass
+
+    def __abs__(self):
+        pass
+
+    
 
 if __name__ == "__main__":
     main()
