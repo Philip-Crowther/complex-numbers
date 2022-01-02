b01d753b6f96c85d978410a3989a1829881874a1
Philip Crowther <crowtherpb@gmail.com>
2022-01-02 14:46:32 -0500

    add representations

--- complex_number.py
+++ complex_number.py
@@ -1,10 +1,17 @@
 #!/usr/bin/env python
 
-class complex_number:
+class ComplexNumber:
     """representation of a complex number"""
     def __init__(self, real_part, imaginary_part):
+        self.real = real_part
+        self.imaginary = imaginary_part
 
-        pass
+    def __repr__(self):
+        return f"ComplexNumber(real='{self.real}', imaginary={self.imaginary})"
+
+    def __str__(self) -> str:
+        return f"{self.real} + {self.imaginary}i"
+    
 
 
 if __name__ == "__main__":
