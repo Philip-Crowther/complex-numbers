8f8fe4650edc028255be906886e1199264172d76
Philip Crowther <crowtherpb@gmail.com>
2022-01-31 17:30:45 -0500

    improve docstrings

--- complex_number.py
+++ complex_number.py
@@ -16,38 +16,49 @@ class ComplexNumber:
         """constructs complex number from angle and magnitude"""
         pass
 
+
     @property
     def angle(self):
         pass
 
     @property
     def magnitude(self):
-        pass
+        """returns the magnitude of complex number"""
+        return (self.real ** 2 + self.imaginary ** 2) ** 1/2
 
     def __repr__(self):
+        """returns a representation of the complex number"""
         operator = "+" if self.imaginary >= 0 else "-"
         return f"{self.real} {operator} {abs(self.imaginary)}"
 
     def __str__(self) -> str:
+        """returns a string representation of a comlex number"""
         operator = "+" if self.imaginary >= 0 else "-"
         return f"{self.real} {operator} {abs(self.imaginary)}i"
 
     def __add__(self, addend):
+        """implementation of complex addition"""
         pass
 
     def __sub__(self, subtrahend):
+        """implementation of complex subtraction"""
         pass
 
     def __mul__(self, factor):
+        """implementation of complex multiplication 
+        (scale by magnitude and add angles)"""
         new_magnitude = self.magnitude * factor.magnitude
         new_angel = self.angle + factor.angle
         return ComplexNumber.from_angle(new_angel, new_magnitude)
 
     def __truediv__(self, divisor):
+        """Implementation of complex division
+        (shrink by magnitude and subtract angles)"""
         pass
 
     def __abs__(self):
-        pass
+        """return absolute value of the complex number"""
+        return self.magnitude
 
     
 
